955abe82425149402436f1e734546c57be7c2fa3
gzampakikas <giorgoszampakikas@gmail.com>
2026-05-29 14:45:52 +0300

    feat: Rating card

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 function App() {
   return (
-    <>
-
-    </>
+    <div className="
+      max-w-360
+      h-full
+      bg-[hsl(213,19%,18%)]
+      rounded-sm
+      text-white
+    ">
+      hi
+    </div>
   )
 }
 
