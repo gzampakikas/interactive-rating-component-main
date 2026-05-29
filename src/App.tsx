5bfbd2938172e721a65dd3209de7703b0e068a29
gzampakikas <giorgoszampakikas@gmail.com>
2026-05-29 21:41:34 +0300

    feat: App card structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import iconStar from '../images/icon-star.svg';
+
 function App() {
   return (
     <div className="
@@ -7,9 +9,28 @@ function App() {
       rounded-sm
       text-white
     ">
-      hi
+      <img src={iconStar} alt="Icon Star" />
+
+      <span className="text-[hsl(0,100%,100%) text-4xl">How did we do?</span>
+
+      <p>
+        Please let us know how we did with your support
+        request. All feedback is apprecieted to help us 
+        improve our offering!
+      </p>
+
+      <div className="btns">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </div>
+
+      <button>SUBMIT</button>
     </div>
   )
 }
 
 export default App;
+
