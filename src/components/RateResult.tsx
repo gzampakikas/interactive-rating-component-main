777d5b257fedc0d6fb71623fe89b123be8a036fd
gzampakikas <giorgoszampakikas@gmail.com>
2026-06-05 15:08:50 +0300

    RateResult component

--- src/components/RateResult.tsx
+++ src/components/RateResult.tsx
@@ -1,5 +1,48 @@
+import { useLocation } from 'react-router';
+import IllustrationIcon from '../../images/illustration-thank-you.svg';
+
 export default function RateResult() {
+    const location = useLocation();
+    const rating = location.state!.rating;
+
     return(
-        <h1>Hi</h1>
+        <div
+            className="
+                max-w-360
+                h-full
+                bg-grey-900
+                rounded-2xl
+                text-white
+                flex
+                flex-col
+                justify-center
+                items-center
+                gap-4
+                p-10
+                w-160
+            "
+        >
+            <img 
+                src={IllustrationIcon}
+                alt="Illustration Thank You" 
+                className="w-50"
+            />
+
+            <span className="
+                bg-gray-700 
+                rounded-2xl
+                p-2
+                text-orange-500
+            ">
+                You selected {rating} out of 5
+            </span>
+
+            <span className="text-white text-4xl">Thank you!</span>
+
+            <p className="text-grey-500 mx-15">
+                We appreciete you Taking the time to give a rating. If you ever need more 
+                support, don't hesitate to get in touch
+            </p>
+        </div>
     );
 }
